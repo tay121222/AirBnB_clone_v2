ee90ac104e6f5107e665c758adda31e121f97153
Enoch Taylor <tay121222@gmail.com>
2023-09-20 10:22:51 +0000

    Fix console_py pycodestyle check

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -146,7 +146,7 @@ class HBNBCommand(cmd.Cmd):
                     continue
 
             obj_attrs[key] = value
-        
+
         new_instance = HBNBCommand.classes[class_name](**obj_attrs)
         storage.new(new_instance)
         storage.save()
@@ -213,7 +213,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -345,6 +345,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
