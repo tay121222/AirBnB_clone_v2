4e3a6bf27d0b5b6f0173c7dbba1dec662f3a55b7
Enoch Taylor <tay121222@gmail.com>
2023-10-05 23:39:03 +0000

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Fabric script that distributes an archive to your web servers"""
+
+from fabric.api import env, run, put
+from os.path import exists
+
+env.hosts = ['100.26.20.143', '100.26.237.112']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
+
+
+def do_deploy(archive_path):
+    """script distributes an archive to your web servers"""
+    if not exists(archive_path):
+        return False
+
+    try:
+        put(archive_path, '/tmp/')
+        archive_filename = archive_path.split('/')[-1].split('.')[0]
+        run('mkdir -p /data/web_static/releases/{}/'.format(archive_filename))
+        run('tar -xzf /tmp/{}.tgz '
+            '-C /data/web_static/releases/{} '
+            '--strip-components=1'.format(
+                archive_filename, archive_filename
+                )
+            )
+        run('rm /tmp/{}.tgz'.format(archive_filename))
+        run('rm -f /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ '
+            '/data/web_static/current'.format(archive_filename))
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
