4bc3b3196f4447f1cf0d944134221e6587ff866b
Enoch Taylor <tay121222@gmail.com>
2023-10-09 14:38:14 +0000

    Updated task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """Fabric script that distributes an archive to your web servers"""
 
-from fabric.api import env, run, put
-from os.path import exists
+from fabric.api import env, run, put, local
+from os.path import exists, basename, splitext
 from datetime import datetime
 
 env.hosts = ['100.26.20.143', '100.26.237.112']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
-
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static"""
@@ -29,30 +26,28 @@ def do_deploy(archive_path):
         return False
 
     try:
+        file_name = basename(archive_path)
+        base_name, _ = splitext(file_name)
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        archive_filename = archive_path.split('/')[-1].split('.')[0]
-        run('mkdir -p /data/web_static/releases/{}/'.format(archive_filename))
-        run('tar -xzf /tmp/{}.tgz '
-            '-C /data/web_static/releases/{} '
-            '--strip-components=1'.format(
-                archive_filename, archive_filename
-                )
-            )
-        run('rm /tmp/{}.tgz'.format(archive_filename))
-        run('rm -f /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ '
-            '/data/web_static/current'.format(archive_filename))
+        run("rm -rf {}{}/".format(path, base_name))
+        run("mkdir -p {}{}/".format(path, base_name))
+        run("tar -xzf /tmp/{} -C {}{}/".format(file_name, path, base_name))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, base_name))
+        run("rm -rf {}{}/web_static".format(path, base_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(path, base_name))
         print("New version deployed!")
         return True
     except Exception as e:
-        print(e)
         return False
 
 
 def deploy():
     """creates and distributes an archive to your web servers"""
-    archive_path = do_pack()
-    if not archive_path:
+    try:
+        path = do_pack()
+        return do_deploy(path)
+    except:
         return False
-
-    return do_deploy(archive_path)
