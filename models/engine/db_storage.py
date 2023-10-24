1b4872defef21b28fd12f30bef6948bf4a57a23e
Enoch Taylor <tay121222@gmail.com>
2023-10-24 12:08:04 +0000

    updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,10 +73,9 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         Session = scoped_session(sessionmaker(bind=self.__engine,
                                               expire_on_commit=False))
-        self.__session = scoped_session(Session)
+        self.__session = Session()
 
     def close(self):
         """Closes the current session"""
         self.__session.close()
-        Session = scoped_session(sessionmaker(bind=self.__engine,
-                                              expire_on_commit=False))
+        self.__session = None
